9c135c00f56133c163251b2a891bd02d3916c406
Hieu Duong <hieu@qhduong.com>
2014-08-14 08:57:40 -0700

    Updated logic to capture limit param parsing
    Gracefully ignore non-int input for limit. Let user know their acronym
    does not have lfs.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -9,27 +9,46 @@ class IndexController < ApplicationController
 
 	def search
 		acronymn = params[:sf]
-		limit    = params[:limit].to_i
+		limit    = params[:limit] 
+		result   = []
 
-		url = URI.parse("http://www.nactem.ac.uk/software/acromine/dictionary.py?sf=#{acronymn}")
+		# get int from limit
+		if is_int_string(limit)
+			limit = limit.to_i
+		else 
+			limit = nil
+		end
+			
+		if acronymn.blank?
+			render "result", :layout => "application", :locals => {:result => result}
+			return
+		end
+
+		url      = URI.parse("http://www.nactem.ac.uk/software/acromine/dictionary.py?sf=#{acronymn}")
 		request  = Net::HTTP::Get.new(url.to_s)
 		response = Net::HTTP.start(url.host, url.port) {|http|
 		  http.request(request)
 		}
 
-		json   = JSON.parse response.body
-		result = []
-
+		json = JSON.parse(response.body)
+		
 		unless json.empty? 
 			json.each do |element| # each element in json response
 				element['lfs'].each do |each_lfs| # get the lfs of each array element
-					break if result.size >= limit
+					break unless limit.nil? or result.size < limit
 					result.push each_lfs['lf'] # get the actual long form
 				end
 			end
 		end 
-		@result = result
+		
+		render "result", :layout => "application", :locals => {:result => result}
+	end
 
-		render "result", :layout => "application"
+	private 
+
+	def is_int_string(string)
+		return if string.blank?
+
+		return !string.match(/^\d+$/).nil?
 	end
 end
